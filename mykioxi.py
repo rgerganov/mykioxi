434467f53dcbbb45fce27c1120464196cef409bb
Radoslav Gerganov <rgerganov@gmail.com>
2020-11-17 10:11:34 +0200

    Add multiline output

--- mykioxi.py
+++ mykioxi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import asyncio
 import argparse
 import sys
@@ -7,23 +8,35 @@ import bleak
 
 CHARACTERISTIC_UUID = "49535343-1e4d-4bd9-ba61-23c647249616"
 
-class LogHandler:
+class DataPrinter:
     def __init__(self):
         self.last_bpm = -1
         self.last_spo2 = -1
+        self.last_pleth = -1
 
-    def data_handler(self, bpm, spo2):
+    def multiline(self, pleth, bpm, spo2):
         if bpm != self.last_bpm or spo2 != self.last_spo2:
             print_bpm = '---' if bpm == 255 else str(bpm).rjust(3)
             print_spo2 = '---' if spo2 == 127 else str(spo2).rjust(3)
-            print("BPM: {}, SpO2: {}".format(print_bpm, print_spo2))
+            now = datetime.datetime.now()
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+            print("[{}]\tBPM: {}\tSpO2: {}".format(timestamp, print_bpm, print_spo2))
             self.last_bpm, self.last_spo2 = bpm, spo2
+
+    def oneline(self, pleth, bpm, spo2):
+        if pleth != self.last_pleth or bpm != self.last_bpm or spo2 != self.last_spo2:
+            print_bpm = '---' if bpm == 255 else str(bpm).rjust(3)
+            print_spo2 = '---' if spo2 == 127 else str(spo2).rjust(3)
+            print_pleth = ('*' * (pleth // 10)).ljust(10)
+            sys.stdout.write("BPM:{}  [{}]  SpO2:{}\r".format(print_bpm, print_pleth, print_spo2))
+            self.last_pleth, self.last_bpm, self.last_spo2 = pleth, bpm, spo2
 
 def make_handler(user_handler):
     def raw_handler(sender, data):
+        pleth = data[1]
         bpm = data[3] | ((data[2] & 64) << 1)
         spo2 = data[4]
-        user_handler(bpm, spo2)
+        user_handler(pleth, bpm, spo2)
     return raw_handler
 
 async def discover():
@@ -45,6 +58,7 @@ async def read_data(address, done, handler):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--device", help="Device address")
+    parser.add_argument("--multiline", help="Multiline data output", action="store_true")
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
@@ -59,9 +73,11 @@ def main():
     else:
         address = args.device
 
+    dp = DataPrinter()
+    printer = dp.multiline if args.multiline else dp.oneline
+
     done = asyncio.Event()
-    lh = LogHandler()
-    task = loop.create_task(read_data(address, done, lh.data_handler))
+    task = loop.create_task(read_data(address, done, printer))
     try:
         loop.run_until_complete(task)
     except KeyboardInterrupt:
